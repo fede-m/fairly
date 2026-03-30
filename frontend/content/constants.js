5589dcc862cf84ce2670a770293d69c7c81f585c
jacopo gili <gilijacopo@gmail.com>
2026-03-30 15:30:54 +0200

    prettified

--- frontend/content/constants.js
+++ frontend/content/constants.js
@@ -1,11 +1,36 @@
 const STRATEGIES = Object.freeze(
-    { 
-        CV: {id: "CV", name: "Doppia forma (M/F)", nestedOptions: ["gli studenti e le studenti", "i/le studenti"]} , 
-        CO: {id: "CO", name: "Nome astratto", nestedOptions: ["la comunità studentesca"]}, 
-        IO: {id: "IO", name: "Forme innovative", nestedOptions: ["l* student*", "l@ student@", "lx studentx", "lu studentu", "lə studentə"]}, 
-        IV: {id: "IV", name: "Tripla forma (M/F/N)", nestedOptions: ["gli studenti, le studenti e l* student*", "gli/le/l* student*"]} 
-    });
+  {
+    CV: {
+      id: "CV",
+      name: "Doppia forma (M/F)",
+      nestedOptions: [
+        "gli studenti e le studenti",
+        "i/le studenti"
+      ]
+    },
+    CO: {
+      id: "CO",
+      name: "Nome astratto",
+      nestedOptions: ["la comunità studentesca"]
+    },
+    IO: {
+      id: "IO",
+      name: "Forme innovative",
+      nestedOptions: [
+        "l* student*",
+        "l@ student@",
+        "lx studentx",
+        "lu studentu",
+        "lə studentə"
+      ]
+    },
+    IV: {
+      id: "IV",
+      name: "Tripla forma (M/F/N)",
+      nestedOptions: [
+        "gli studenti, le studenti e l* student*",
+        "gli/le/l* student*"
+      ]
+    }
+  });
 const DOMAINS = ["unito.it"]
-
-
-    
