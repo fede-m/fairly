fc6fd05e1e9c1747ec4530be4ac44bb6e497f111
jacopo gili <gilijacopo@gmail.com>
2026-04-07 14:03:13 +0200

    first version of the ? button for additional informations on strategies. needs touches. keyboard flow works

--- frontend/content/constants.js
+++ frontend/content/constants.js
@@ -6,12 +6,14 @@ const STRATEGIES = Object.freeze(
       nestedOptions: [
         "gli studenti e le studenti",
         "i/le studenti"
-      ]
+      ],
+      info: "doppia forma Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet tincidunt lorem. Etiam ac faucibus mi. Quisque vitae sem vitae augue ultrices lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at pretium ligula. "
     },
     CO: {
       id: "CO",
       name: "Nome astratto",
-      nestedOptions: ["la comunità studentesca"]
+      nestedOptions: ["la comunità studentesca"],
+      info: "nome astratto Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet tincidunt lorem. Etiam ac faucibus mi. Quisque vitae sem vitae augue ultrices lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at pretium ligula. "
     },
     IO: {
       id: "IO",
@@ -22,7 +24,8 @@ const STRATEGIES = Object.freeze(
         "lx studentx",
         "lu studentu",
         "lə studentə"
-      ]
+      ],
+      info: "forme innovative Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet tincidunt lorem. Etiam ac faucibus mi. Quisque vitae sem vitae augue ultrices lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at pretium ligula. "
     },
     IV: {
       id: "IV",
@@ -30,7 +33,8 @@ const STRATEGIES = Object.freeze(
       nestedOptions: [
         "gli studenti, le studenti e l* student*",
         "gli/le/l* student*"
-      ]
+      ],
+      info: "tripla forma Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet tincidunt lorem. Etiam ac faucibus mi. Quisque vitae sem vitae augue ultrices lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec at pretium ligula. "
     }
   });
 const DOMAINS = ["unito.it"]
