9b4d116b3ae2cb4b8dfa2cdc9b41669b90c0f2c9
jacopo gili <gilijacopo@gmail.com>
2026-04-17 16:32:53 +0200

    commented out db part to run backend

--- backend/database.py
+++ backend/database.py
@@ -6,6 +6,7 @@ import hmac
 import hashlib
 import certifi
 from models import EventRequest
+
 # import dns.resolver
 # dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
 # dns.resolver.default_resolver.nameservers=['8.8.8.8']
@@ -14,38 +15,39 @@ load_dotenv()
 import ssl
 
 # Get secret key for hashing
-SECRET_KEY = os.environ.get("SECRET_KEY")
-password = urllib.parse.quote_plus(os.environ.get("MONGODB_PWD"))
-user = os.environ.get("MONGODB_USER")
-MONGODB_URI = f"mongodb+srv://{user}:{password}@fairlycluster.khg90i7.mongodb.net/?appName=FairlyCluster"
-
-# Connect to the MongoDB Cluster
-client = MongoClient(MONGODB_URI,
-
-                    tls=True,
-                    tlsCAFile=certifi.where(),
-                    serverSelectionTimeoutMS=30000,
-                     )
-
-dbs = client.list_database_names()
-print(dbs)
-
-db = client["fairly_db"]
-collection = db["user_events"]
-
-
-def insert_event(events: list[EventRequest]):
-    for event in events:
-        # Hash the user id
-        email = event.user_id.strip().lower()
-        event.user_id = hmac.new(
-            SECRET_KEY.encode(),
-            email.encode(),
-            hashlib.sha256
-        ).hexdigest()
-
-        inserted_id = collection.insert_one(event.dict()).inserted_id
-
-
-
-
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+# password = urllib.parse.quote_plus(os.environ.get("MONGODB_PWD"))
+# user = os.environ.get("MONGODB_USER")
+# MONGODB_URI = f"mongodb+srv://{user}:{password}@fairlycluster.khg90i7.mongodb.net/?appName=FairlyCluster"
+#
+## Connect to the MongoDB Cluster
+# client = MongoClient(MONGODB_URI,
+#
+#                    tls=True,
+#                    tlsCAFile=certifi.where(),
+#                    serverSelectionTimeoutMS=30000,
+#                     )
+#
+# dbs = client.list_database_names()
+# print(dbs)
+#
+# db = client["fairly_db"]
+# collection = db["user_events"]
+#
+#
+# def insert_event(events: list[EventRequest]):
+#    for event in events:
+#        # Hash the user id
+#        email = event.user_id.strip().lower()
+#        event.user_id = hmac.new(
+#            SECRET_KEY.encode(),
+#            email.encode(),
+#            hashlib.sha256
+#        ).hexdigest()
+#
+#        inserted_id = collection.insert_one(event.dict()).inserted_id
+#
+#
+#
+#
+#
