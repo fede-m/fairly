a315ec30b6314898a3460d182c10615148758878
jacopo gili <gilijacopo@gmail.com>
2026-06-15 15:55:43 +0200

    swapped localstorage to chrome.storage.local

--- frontend/contentScript.js
+++ frontend/contentScript.js
@@ -62,17 +62,17 @@ function initializeSession() {
 
   try {
     // Get strategies order
-    const strategyOrder = localStorage.getItem("fairlyStrategyOrder");
+    const strategyOrder = chrome.storage.local.getItem("fairlyStrategyOrder");
     if (strategyOrder) {
       STRATEGY_ORDER = JSON.parse(strategyOrder);
     } else {
       const strategies = Object.keys(STRATEGIES);
       const randomizedOrder = [...strategies].sort(() => Math.random() - 0.5)
-      localStorage.setItem("fairlyStrategyOrder", JSON.stringify(randomizedOrder));
+      chrome.storage.local.setItem("fairlyStrategyOrder", JSON.stringify(randomizedOrder));
       STRATEGY_ORDER = randomizedOrder;
     }
   } catch (e) {
-    console.warn("localStorage unavailable, using random strategy order.", e);
+    console.warn("chrome.storage.local unavailable, using random strategy order.", e);
     STRATEGY_ORDER = Object.keys(STRATEGIES).sort(() => Math.random() - 0.5);
   }
 
@@ -214,7 +214,7 @@ function startAnalysis() {
       dataObj["session_id"] = SESSION_ID;
       dataObj["user_id"] = USER_EMAIL;
       // remember the selected options for future uses
-      localStorage.setItem("fairlyLastStrategy", selected.id);
+      chrome.storage.local.setItem("fairlyLastStrategy", selected.id);
 
       editableElements.forEach((element) => {
         const data = {};
@@ -322,7 +322,7 @@ function createInfoDiv() {
   infoDiv.appendChild(paragraph);
 
   // last used strategy
-  const savedStrategy = localStorage.getItem("fairlyLastStrategy");
+  const savedStrategy = chrome.storage.local.getItem("fairlyLastStrategy");
 
   // Check-list container 
   const checklist = document.createElement("div");
